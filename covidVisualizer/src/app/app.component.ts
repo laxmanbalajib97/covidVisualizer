8404cfa2b84edb4dd984ecbb000124301ea6538c
Laxman Balaji Balasubramanian <laxmanbalaji97@gmail.com>
2020-04-20 01:20:12 -0500

    integrated time series US data

--- covidVisualizer/src/app/app.component.ts
+++ covidVisualizer/src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { CovidDataService } from './covid-data.service';
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'covidVisualizer';
+  chart = []; // This will hold our chart info
+
+  constructor(private _coviddata: CovidDataService) {}
+
+  ngOnInit() {
+    this._coviddata.timeSeriesCovidData().subscribe((res) => {
+      let ctx = document.getElementById('myChart');
+
+      let US_data = res['US'].map((res) => res.confirmed);
+      let Date_data = res['US'].map((res) => res.date);
+
+      this.chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: Date_data,
+          datasets: [
+            {
+              data: US_data,
+            },
+          ],
+        },
+      });
+    });
+  }
 }
--- covidVisualizer/src/app/covid-data.service.ts
+++ covidVisualizer/src/app/covid-data.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CovidDataService {
-
-  constructor() { }
+  constructor(private _http: HttpClient) {}
+  timeSeriesCovidData() {
+    return this._http
+      .get('https://pomber.github.io/covid19/timeseries.json')
+      .pipe(map((result) => result));
+  }
 }
